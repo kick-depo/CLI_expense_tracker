2a6d950ad6c546e70d39047a724a14a21a07c520
Bogdan Romanov <kaperlinkor@yandex.ru>
2025-12-08 00:11:58 +0300

    it's work! But broken...

--- expense.py
+++ expense.py
@@ -8,30 +8,33 @@ parser.add_argument("--amount", "-a", type=int, help="Сумма расхода"
 
 args = parser.parse_args()
 
-rows = [['ID', 'Date', 'Description', 'amount']]
+field = ['ID', 'Date', 'Description', 'amount']
+
+def exp_add():
+    description = args.description
+    amount = args.amount
+    id = get_id()
+    date = get_date()
+    with open('database.csv', 'a') as file:
+        new_row = [[id, date, description, amount]]
+        csv_writer = csv.writer(file)
+        csv_writer.writerows(new_row)
+
+
+# нужна оптимизация
+def get_id() -> str:
+    with open("database.csv", "r") as file:
+        csv_dict_reader = csv.DictReader(file)
+        temp = [0]
+        for row in csv_dict_reader:
+            temp.append(row["ID"])
+            # if row["ID"]-1 != temp[-1]:
+            #     break
+        return str(temp[-1])
+
+
+def get_date() -> str:
+    return "2025"
 
 if args.command == "add":
-    def exp_add():
-        description = args.description
-        amount = args.amount
-        id = get_id()
-        date = get_date()
-        with open('database.csv', 'a') as file:
-            new_row = [id, date, description, amount]
-            csv_writer = csv.writer(file)
-
-        pass
-
-    # нужна оптимизация
-    def get_id():
-        with open("database.csv", "r") as file:
-            csv_dict_reader = csv.DictReader(file)
-            temp = []
-            for row in csv_dict_reader:
-                temp.append(row["ID"])
-                if row["ID"]-1 != temp[-1]:
-                    break
-        return temp[-1]
-
-    def get_date():
-        return 2025
+    exp_add()
